b9a1674bbd7dde35ef1e6a81dde5123242983039
Sachin Sharma <sspl.sachin2022@gmail.com>
2026-05-21 19:02:39 +0530

    main landing page changes
    matched 5 sections with figma accurately

--- js/custom.js
+++ js/custom.js
@@ -2,28 +2,23 @@
 //  Featured Listings Swiper
 // ================================================
 const listingsSwiper = new Swiper('.listings-swiper', {
-    slidesPerView: 1,
+    slidesPerView: 1.1,
     spaceBetween: 24,
     slidesPerGroup: 1,
     loop: true,
     grabCursor: true,
     navigation: {
-        prevEl: '#listingsPrev',
         nextEl: '#listingsNext',
-    },
-    pagination: {
-        el: '.listings-pagination',
-        clickable: true,
     },
     breakpoints: {
         576: {
-            slidesPerView: 1.2,
+            slidesPerView: 1.5,
         },
         768: {
-            slidesPerView: 2,
+            slidesPerView: 2.2,
         },
         1200: {
-            slidesPerView: 2.5,
+            slidesPerView: 3.2,
         },
     },
 });
